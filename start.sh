cc6291b6998a976dce79073c7753890eec6f3416
samurai-maker <84018991+samurai-maker@users.noreply.github.com>
2022-10-24 01:40:40 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/TR-TECH-GUIDE/Amanda-Music-v2.git /Amanda-Music-v2
+git clone https://github.com/samurai-maker/Amanda-Music.git /Amanda-Music-v2
 cd /Amanda-Music-v2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
